fb7f86310de1747c366373a404ed7cd54f55afea
ErikAndreasKlokk <eaklokk@gmail.com>
2024-03-21 15:09:10 +0100

    hehe

--- src/routes/watermark.svelte
+++ src/routes/watermark.svelte
@@ -1,1 +1,1 @@
-<p class=" fixed bottom-0 left-0 font-bold text-gray-900/0 p-2 z-50 ">copyright @ Erik Andreas Klokk</p>
+<p class=" fixed bottom-0 left-0 font-bold text-gray-900/70 p-2 z-50 ">copyright @ Erik Andreas Klokk</p>
