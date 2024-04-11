1beeac4bf09460bc0038a0cb63cdb647f039314d
Carlo331 <davidcarlanton06@gmail.com>
2024-04-11 13:21:13 +0200

    ferdig

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,32 +3,37 @@
 </script>
 
 <div class=" snap-y snap-mandatory overflow-y-scroll h-screen w-screen scroll-smooth">
-    <section class=" h-screen w-screen snap-always snap-center flex justify-center items-center bg-cover bg-center" style="background-image: url('/flaske1.jpg');">
-        <p class=" font-bold text-9xl text-red-500">FLASKER PÅ IM</p>
+    <section class=" h-screen w-screen bg-[url('/langt_unna.jpg')] bg-cover bg-bottom snap-always snap-center bg-slate-600 flex justify-center items-center">
+        <p class=" font-bold text-zinc-100 text-9xl">IM på jobb</p>
     </section>
-    <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske2.jpg');">
-        <div class=" absolute bottom-40 right-60 w-80 h-20 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Viktor. Den er Stor.</p>
+    <section class=" h-screen w-full bg-[url('/nerme_høyre.jpg')] bg-cover bg-top snap-always snap-center bg-gray-400 relative">
+        <div class=" absolute bottom-40 right-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Her ser vi erik hoste norsk oppgaven på versell. Virkelig et sjeldent syn.</p>
         </div>
     </section>
-    <section class=" h-screen w-screen snap-always snap-center relative bg-cover bg-center" style="background-image: url('/flaske3.jpg');">
-        <div class=" absolute bottom-40 left-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Alvar. Det er en usunn monster.</p>
+    <section class=" h-screen w-screen bg-[url('/over.jpg')] bg-cover bg-bottom snap-always snap-center bg-slate-600 relative">
+        <div class=" absolute bottom-40 left-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Her ser vi Erik fra en ny vinkel. Dette er helt villt.</p>
         </div>
     </section>
-    <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske4.jpg');">
-        <div class=" absolute bottom-40 right-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Dev. Den er veldig fancy.</p>
+    <section class=" h-screen w-screen bg-[url('/bak.jpg')] bg-cover bg-bottom snap-always snap-center bg-slate-600 relative">
+        <div class=" absolute bottom-40 left-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Erik står på, han skal bli ferdig med denne oppgaven, men blir han?</p>
         </div>
     </section>
-    <section class=" h-screen w-screen snap-always snap-center relative bg-cover bg-center" style="background-image: url('/flaske5.jpg');">
-        <div class=" absolute bottom-40 left-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Helena. Den er veldig fargerik.</p>
+    <section class=" h-screen w-screen bg-[url('/slaveri.jpg')] bg-cover bg-top snap-always snap-center bg-slate-600 relative">
+        <div class=" absolute bottom-40 left-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Lukas har funnet et nytt konsept. Kanskje dette starter noe fint.</p>
         </div>
     </section>
-    <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske6.jpg');">
-        <div class=" absolute bottom-40 right-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til lærer Madeleine. Den er en klassiker.</p>
+    <section class=" h-screen w-screen bg-[url('/bilde.jpg')] bg-cover bg-top snap-always snap-center bg-slate-600 relative">
+        <div class=" absolute bottom-40 left-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Det ser ut som det ikke bare er Erik som er på jobb.</p>
+        </div>
+    </section>
+    <section class=" h-screen w-screen bg-[url('/pose.jpg')] bg-cover bg-top snap-always snap-center bg-slate-600 relative">
+        <div class=" absolute bottom-40 left-60 w-80 h-40 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Erik poser for kamera.</p>
         </div>
     </section>
 </div>
