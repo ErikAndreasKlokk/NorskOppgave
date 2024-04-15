6c7bc7d62f55e3fab457a7990e9174be5ddf4462
ErikAndreasKlokk <eaklokk@gmail.com>
2024-04-15 10:29:10 +0200

    oppdatert tekst

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,28 +7,28 @@
         <p class=" font-bold text-9xl text-red-500">FLASKER PÅ IM</p>
     </section>
     <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske2.jpg');">
-        <div class=" absolute bottom-40 right-60 w-80 h-20 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Viktor. Den er Stor.</p>
+        <div class=" absolute bottom-40 right-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
+            <p class=" font-semibold">Flasken til Viktor er en av de mest populære flaskene på markedet. Det er flere IM elever som bruker denne.</p>
         </div>
     </section>
     <section class=" h-screen w-screen snap-always snap-center relative bg-cover bg-center" style="background-image: url('/flaske3.jpg');">
         <div class=" absolute bottom-40 left-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Alvar. Det er en usunn monster.</p>
+            <p class=" font-semibold">Flasken til Alvar er en Monster kanne, med smaken mango loco. Monster er enda en veldig populær drikke på IM.</p>
         </div>
     </section>
     <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske4.jpg');">
         <div class=" absolute bottom-40 right-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Dev. Den er veldig fancy.</p>
+            <p class=" font-semibold">Dev har en veldig fancy flaske. Den har et minimalt design og passer godt inn på IM.</p>
         </div>
     </section>
     <section class=" h-screen w-screen snap-always snap-center relative bg-cover bg-center" style="background-image: url('/flaske5.jpg');">
         <div class=" absolute bottom-40 left-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til Helena. Den er veldig fargerik.</p>
+            <p class=" font-semibold">Flasken til Helena er samme type som Viktor sinn, men en mer fargerik utgave.</p>
         </div>
     </section>
     <section class=" h-screen w-full snap-always snap-center  relative bg-cover bg-center" style="background-image: url('/flaske6.jpg');">
         <div class=" absolute bottom-40 right-60 w-80 h-28 bg-red-500 flex justify-center items-center p-3 rounded-lg">
-            <p class=" font-semibold">Her er flasken til lærer Madeleine. Den er en klassiker.</p>
+            <p class=" font-semibold">Madeleine sin flaske er en klassiker. Camelbak er et av de største merkene på flaske markedet.</p>
         </div>
     </section>
 </div>
